0ef275b9c533c3dcf1f7507da5976c7c34a0847a
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-02-09 19:41:52 +0000

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,5 @@
+// Import the runStartQuestions function from the teamquestion.js file
+const startMainQuestions = require('./src/teamquestion')
+
+// Invoke the runStartQuestions function
+startMainQuestions();
