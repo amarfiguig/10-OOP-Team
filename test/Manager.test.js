de3a038fb11abdf6195a850cc8921101939a904e
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-02-12 09:04:07 +0000

    Update Manager.test.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,5 +1,5 @@
 // Requiring the information from the "Manager" class
-const Employee = require("../lib/Manager");
+const Employee = require('../lib/Manager');
 
 // Defining a test function for the "Manager" class
         describe("Manager", () => {
@@ -12,26 +12,26 @@ const Employee = require("../lib/Manager");
                 
 
 //create expected example inputs 
-        const id = U24478;
-        const name = "X";
-        const email = "x@work.com";
+        const name = 'X';
+        const id = 'U24478';
+        const email = 'x@work.com';
         const officeNumber = '03325698745';
-        const role = "Manager";
+        const role = 'Manager';
         
-//create a new instance object using the intern class
-        const object = new intern(id, name, email, officeNumber);
+//create a new instance team using the intern class
+        const team = new Manager(id, name, email, officeNumber);
         
 // Check if the properties of the Intern object match the expected outputs
         expect(team.name).toEqual(name);
         expect(team.id).toEqual(id);
         expect(team.email).toEqual(email);
-        expect(team.school).toEqual(officeNumber);
+        expect(team.officeNumber).toEqual(officeNumber);
 
 // Check if the getter methods of the Intern object match the expected outputs
         expect(team.getName()).toEqual(name);
-        expect(team.getID()).toEqual(id);
+        expect(team.getId()).toEqual(id);
         expect(team.getEmail()).toEqual(email);
-        expect(team.getSchool()).toEqual(officeNumber);
+        expect(team.getOfficeNumber()).toEqual(officeNumber);
 
        })
     })
