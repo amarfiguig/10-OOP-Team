a89ae115ebe1c15a6f6a8a10558a1facd16c96e5
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-02-12 09:00:49 +0000

    Update Intern.test.js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,5 +1,5 @@
-// Requiring the information from the "Employee" class
-const Employee = require("../lib/Intern");
+// Requiring the information from the "Intern" class
+const Intern = require('../lib/Intern');
 
 // Defining a test function for the "Intern" class
 describe("Intern", () => {
@@ -10,30 +10,27 @@ describe("Intern", () => {
 // Defining a test case to check if the output of the constructor matches the expected values
     it("Verifying Intern Information", () => {
             
-            
-
         //create expected example inputs 
-        const id = TR589365;
-        const name = "X";
-        const email = "x3@work.com";
-        const role = "Intern";
-        const school = "CodeX";
+        const name = 'X';
+        const id = 'TR589365';
+        const email = 'x3@work.com';
+        const school = 'CodeX';
 
-        //create a new instance object using the intern class
-        const object = new intern(id, name, email, school);
+        //create a new instance team using the intern class
+        const team = new Intern(id, name, email, school);
         
-// Check if the properties of the Intern object match the expected outputs
-  expect(team.name).toEqual(name);
-  expect(team.id).toEqual(id);
-  expect(team.email).toEqual(email);
-  expect(team.school).toEqual(school);
+// Check if the properties of the Intern team match the expected outputs
+        expect(team.name).toEqual(name);
+        expect(team.id).toEqual(id);
+        expect(team.email).toEqual(email);
+        expect(team.school).toEqual(school);
 
 // Check if the getter methods of the Intern object match the expected outputs
-  expect(team.getName()).toEqual(name);
-  expect(team.getID()).toEqual(id);
-  expect(team.getEmail()).toEqual(email);
-  expect(team.getSchool()).toEqual(school);
+        expect(team.getName()).toEqual(name);
+        expect(team.getId()).toEqual(id);
+        expect(team.getEmail()).toEqual(email);
+        expect(team.getSchool()).toEqual(school);
 
-       })
     })
+  })
 });
