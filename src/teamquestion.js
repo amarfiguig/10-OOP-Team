0b45136169542a2a3c9419b73d26871bbac6288a
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-02-15 07:53:20 +0000

    Update teamquestion.js

--- src/teamquestion.js
+++ src/teamquestion.js
@@ -1,212 +1,131 @@
-//The questions that will be displayed to the user when the functions are executed.
-const playQuestion = [
-  {
-    type: "list",
-    message: "Choose an action:",
-    name: "selection",
-    choices: ["Add Manager", "Add Engineer", "Add Intern", "Build Team Page"],
+const inquirer = require("inquirer");
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+
+const playQuestions = [  
+  {    type: "list",    
+       message: "Choose an action:",    
+       name: "selection",    
+       choices: ["Add Manager", "Add Engineer", "Add Intern", "Build Team Page"],
   },
 ];
 
-const ManagerQuestion = [
-
-  {
-    type: "input",
-    name: "id",
-    message: "What Employee's ID number ?",
-  },
-  {
-    type: "input",
-    name: "name",
-    message: "What Employee's name ?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What Employee's email ?",
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What Manager's office number ?",
+const ManagerQuestion = [ 
+  {    type: "input",
+       name: "id",    
+       message: "What is the Manager's ID number?",
+  },  
+  {    type: "input",    
+       name: "name",    
+       message: "What is the Manager's name?",  
+  },  
+  {    type: "input",    
+       name: "email",    
+       message: "What is the Manager's email?",  },  
+  {    type: "input",    
+       name: "officeNumber",    
+       message: "What is the Manager's office number?",  
   },
 ];
 
-const EngineerQuestion = [
-  {
-    type: "input",
-    name: "name",
-    message: "WHat Employee's name ?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What Employee's ID number ?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What Employee's email ?",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What Engineer's GitHub username ?",
+const EngineerQuestion = [  
+  {    type: "input",    
+       name: "id",    
+       message: "What is the Engineer's ID number?",  
+  },  
+  {    type: "input",    
+       name: "name",    
+       message: "What is the Engineer's name?",  
+  },  
+  {    type: "input",    
+       name: "email",    
+       message: "What is the Engineer's email?",  
+  },  
+  {    type: "input",    
+       name: "github",    
+       message: "What is the Engineer's GitHub username?",  
   },
 ];
 
-const InternQuestion = [
-  
-  {
-    type: "input",
-    name: "id",
-    message: "Employee's ID number:",
-  },
-  {
-    type: "input",
-    name: "name",
-    message: "What Employee's name ?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What Employee's email ?",
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "WHat Intern's school name ?",
+const InternQuestion = [  
+  {    type: "input",    
+       name: "id",   
+       message: "What is the Intern's ID number?",  
+  },  
+  {    type: "input",    
+       name: "name",    
+       message: "What is the Intern's name?",  
+  },  
+  {    type: "input",   
+       name: "email",   
+       message: "What is the Intern's email?",  
+  },  
+  {    type: "input",    
+       name: "school",   
+       message: "What is the Intern's school name?",  
   },
 ];
 
-module.exports = { playQuestion, InternQuestion, EngineerQuestion, ManagerQuestion };
-        }
+const managerArray = [];
+const engineerArray = [];
+const internArray = [];
 
-        else if(response.action_choice === 'Add a manager'){
-            addManager()
-
-       }
-
-       //else build the team
-       else buildTeam(managerArray, engineerArray, internArray);
-       
-     })
+function starterQuestions() {
+  inquirer.prompt(playQuestions).then((response) => {
+    if (response.selection === "Add Manager") {
+      addManager();
+    } else if (response.selection === "Add Engineer") {
+      addEngineer();
+    } else if (response.selection === "Add Intern") {
+      addIntern();
+    } else {
+      buildTeam(managerArray, engineerArray, internArray);
+    }
+  });
 }
 
-
 function addEngineer() {
-    inquirer.prompt([
-
-        {
-            type: 'input',
-            message: 'What is the engineer\s ID?',
-            name: 'engineer_id',
-
-        },
-        {
-            type: 'input',
-            message: 'What is the engineers\s name?',
-            name: 'engineer_name',
-            
-        },
-        {
-            type: 'input',
-            message: 'What is the engineer\s email?',
-            name: 'engineer_email',
-
-        },
-
-        {
-            type: 'input',
-            message: 'What is the engineer\s GitHub profile name?',
-            name: 'engineer_github',
-
-        },
-    ])
-    .then(response => {
-       
-
-        const engineer = new Engineer(response.engineer_id, response.engineer_name, response.engineer_email, response.engineer_github);
-
-        engineerArray.push(engineer);
-
-        starterQuestions();
-     })
+  inquirer.prompt(EngineerQuestion).then((response) => {
+    const engineer = new Engineer(
+      response.id,
+      response.name,
+      response.email,
+      response.github
+    );
+    engineerArray.push(engineer);
+    starterQuestions();
+  });
 }
 
 function addIntern() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the engineer\s ID?',
-            name: 'intern_id',
-        }
-        {
-            type: 'input',
-            message: 'What is the intern\s name?',
-            name: 'intern_name',
-        },
-        {
-            type: 'input',
-            message: 'What is the intern\s email?',
-            name: 'intern_email',
-        },
-        {
-            type: 'input',
-            message: 'What is the intern\s school name?',
-            name: 'intern_school',
-
-        },
-    ])
-    .then(response => {
-
-        const intern = new Intern(response.intern_id, response.intern_name, response.intern_email, response.intern_school);
-
-        internArray.push(intern);
-
-        playQuestions();
-
-     })
+  inquirer.prompt(InternQuestion).then((response) => {
+    const intern = new Intern(
+      response.id,
+      response.name,
+      response.email,
+      response.school
+    );
+    internArray.push(intern);
+    starterQuestions();
+  });
 }
 
 function addManager() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the manager\s ID?',
-            name: 'manager_id',
-
-        },
-        {
-            type: 'input',
-            message: 'What is the manager\s name?',
-            name: 'manager_name',
-            
-        },
-        {
-            type: 'input',
-            message: 'What is the manager\s email?',
-            name: 'manager_email',
-        },
-        {
-            type: 'input',
-            message: 'What is the manager\s office nubmer?',
-            name: 'manager_officeNubmer',
-
-        },
-    ])
-    .then(response => {
-
-        const manager = new Manager(response.manager_id, response.manager_name, response.manager_email, response.manager_officeNubmer);
-
-        managerArray.push(manager);
-
-        playQuestions();
-    })
+  inquirer.prompt(ManagerQuestion).then((response) => {
+    const manager = new Manager(
+      response.id,
+      response.name,
+      response.email,
+      response.officeNumber
+    );
+    managerArray.push(manager);
+    starterQuestions();
+  });
 }
 
+function buildTeam() {
+  // Function to build team page
+}
 
-
-
-
-module.exports = {playQuestions, addEngineer, addIntern, addManager}
+starterQuestions();
