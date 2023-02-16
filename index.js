5d5404d604c054d97d27377183bd1b2e44b602ab
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-02-16 20:46:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import the PlayQuestions function from the teamquestion.js file
-const startMainQuestions = require('./src/teamquestion')
+const starterQuestions = require('./src/teamquestion')
 
 // Invoke the PlayQuestions function
-startMainQuestions();
+starterQuestions();
